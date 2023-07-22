936d50ffd4499ed7e5036eff4826b6536d7e1564
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-07-22 01:11:45 +0100

    added: initial configuration

---
+++ node/src/app.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(3333, () => console.log(`Server is running on PORT 3333`))
