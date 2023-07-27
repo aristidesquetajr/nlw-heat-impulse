fb8665c7dfc63320eb64163dbdb6db8dfd0daae1
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-07-27 21:54:49 +0100

    added: athentication of user with github

--- node/src/app.ts
+++ node/src/app.ts
@@ -1,9 +1,24 @@
+import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+
+import { router } from './routes'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use(router)
+
+app.get("/github", (request, response) => {
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GIGHUB_CLIENT_ID}`)
+})
+
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query
+
+  return response.json(code)
+})
 
 app.listen(3333, () => console.log(`Server is running on PORT 3333`))
