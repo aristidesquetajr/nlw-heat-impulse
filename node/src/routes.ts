57786446100362f658606efef62dc9be31cbe138
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-07-31 19:51:34 +0100

    added: list 3 last messages

--- node/src/routes.ts
+++ node/src/routes.ts
@@ -2,11 +2,14 @@ import { Router } from 'express'
 import { AuthenticateUserController } from './controllers/AuthenticateUserController'
 import { ensureAuthenticated } from './middleware/ensureAuthenticated'
 import { CreateMessageController } from './controllers/CreateMessageController'
+import { GetLast3MessagesController } from './controllers/GetLast3MessagesController'
 
 const router = Router()
 
 router.post("/authenticate", new AuthenticateUserController().handle)
 
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
+
+router.get("/messages/last3", new GetLast3MessagesController().handle)
 
 export { router }
