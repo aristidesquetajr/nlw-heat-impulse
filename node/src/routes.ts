2989c49d7b4fbc5e1cf746264bb111a596dfdd7c
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-07-31 20:31:52 +0100

    added: user profile

--- node/src/routes.ts
+++ node/src/routes.ts
@@ -3,6 +3,7 @@ import { AuthenticateUserController } from './controllers/AuthenticateUserContro
 import { ensureAuthenticated } from './middleware/ensureAuthenticated'
 import { CreateMessageController } from './controllers/CreateMessageController'
 import { GetLast3MessagesController } from './controllers/GetLast3MessagesController'
+import { ProfileUserController } from './controllers/ProfileUserController'
 
 const router = Router()
 
@@ -11,5 +12,7 @@ router.post("/authenticate", new AuthenticateUserController().handle)
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
 
 router.get("/messages/last3", new GetLast3MessagesController().handle)
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle)
 
 export { router }
