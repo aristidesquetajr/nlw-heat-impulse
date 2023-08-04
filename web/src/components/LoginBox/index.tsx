0aa5b90a828e84d6420c37c148ee5c974f0d69a0
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-08-04 22:59:05 +0100

    added: LoginBox component design

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,5 +1,14 @@
+import { VscGithubInverted } from "react-icons/vsc";
+
 import styles from "./styles.module.scss";
 
 export const LoginBox = () => {
-  return <div className={styles.loginBoxWrapper}>Login</div>;
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href="#" className={styles.signInWithGithub}>
+        <VscGithubInverted size="24" /> Entrar com Github
+      </a>
+    </div>
+  );
 };
